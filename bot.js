b7687adb8bc4c2a4af26b50018b0366da6b15761
rapidofc1 <33822216+rapidofc1@users.noreply.github.com>
2017-11-20 10:14:53 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -25,7 +25,10 @@ client.on('message', message => {
 });
 client.on('message', message => {
     if (message.content === '!botinfo') {
-    	message.channel.send(':information_source: __**Bot Owner:**__ Rapid ~ __**Bot hosted on:**__ Heroku ~ __**Bot Version:**__ 1.0');
+    	message.channel.send(':information_source: Well, here is my info!
+                             __**Bot Owner:**__ Rapid
+                             __**Bot hosted on:**__ Heroku
+                             __**Bot Version:**__ 1.0');
     }
 });
        
@@ -34,7 +37,28 @@ client.on('message', message => {
     	message.reply('BONG!');
   	}
 });
-if (message.content === "!8ball") { 	var sayings = ["It is certain",										"It is decidedly so",										"Without a doubt",										"Yes, definitely",										"You may rely on it",										"As I see it, yes",										"Most likely",										"Outlook good",										"Yes",										"Signs point to yes",										"Reply hazy try again",										"Ask again later",										"Better not tell you now",										"Cannot predict now",										"Concentrate and ask again",										"Don't count on it",										"My reply is no",										"My sources say no",										"Outlook not so good",										"Very doubtful"]; 			var result = Math.floor((Math.random() * sayings.length) + 0);			bot.reply(message, sayings[result]); 
+if (message.content === "!8ball") { 	var sayings = ["It is certain",
+                                                       "It is decidedly so",
+                                                       "Without a doubt",
+                                                       "Yes, definitely",
+                                                       "You may rely on it",
+                                                       "As I see it, yes",
+                                                       "Most likely",
+                                                       "Outlook good",
+                                                       "Yes",
+                                                       "Signs point to yes",
+                                                       "Reply hazy try again",
+                                                       "Ask again later",
+                                                       "Better not tell you now",
+                                                       "Cannot predict now",
+                                                       "Concentrate and ask again",
+                                                       "Don't count on it",
+                                                       "My reply is no",
+                                                       "My sources say no",
+                                                       "Outlook not so good",
+                                                       "Very doubtful"]; 			
+                                   var result = Math.floor((Math.random() * sayings.length) + 0);			
+                                   bot.reply(message, sayings[result]); 
     }
 
 
