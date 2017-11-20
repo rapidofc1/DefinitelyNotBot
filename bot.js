11de106b37ce3d84010a19f7924fc6e9c6845d13
rapidofc1 <33822216+rapidofc1@users.noreply.github.com>
2017-11-20 16:04:04 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -21,18 +21,11 @@ client.on('message', message => {
     if (message.content === '!whoisrapid') {
     	message.channel.send('**The super dank creator and owner of this server, obviously.** :grin:');
     }  
-
 });
 client.on('message', message => {
     if (message.content === '!botinfo') {
     	message.channel.send(':information_source: Well, here is my info! __**Bot Owner:**__ Rapid, __**Bot hosted on:**__ Heroku, __**Bot Version:**__ 1.5');
     }
-
-});
-client.on('message', message => {
-    if (message.content === '!') {
-        message.channel.send ('
-  	}
 });
         
 // THIS  MUST  BE  THIS  WAY
