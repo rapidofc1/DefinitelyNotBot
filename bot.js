a43dbd81f8b757db4c4f3f4b444ba1f1ad4645ee
rapidofc1 <33822216+rapidofc1@users.noreply.github.com>
2017-11-20 08:14:55 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -15,6 +15,12 @@ client.on('message', message => {
     if (message.content === 'ur mom') {
     	message.channel.send('**NO,** your mum.');
   	}
+});
+
+client.on('message', message => {
+    if (message.content === 'whoisrapid') {
+    	message.channel.send('**The super dank creator and owner of this server, obviously.** :SeizureEyes:');
+  	}
     
 });
 
