55299a28a5f8b682393c9d269920fc45d23dee8d
rapidofc1 <33822216+rapidofc1@users.noreply.github.com>
2017-11-20 10:07:58 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -34,6 +34,9 @@ client.on('message', message => {
     	message.reply('BONG!');
   	}
 });
+if (message.content === "!8ball") { 	var sayings = ["It is certain",										"It is decidedly so",										"Without a doubt",										"Yes, definitely",										"You may rely on it",										"As I see it, yes",										"Most likely",										"Outlook good",										"Yes",										"Signs point to yes",										"Reply hazy try again",										"Ask again later",										"Better not tell you now",										"Cannot predict now",										"Concentrate and ask again",										"Don't count on it",										"My reply is no",										"My sources say no",										"Outlook not so good",										"Very doubtful"]; 			var result = Math.floor((Math.random() * sayings.length) + 0);			bot.reply(message, sayings[result]); 
+    }
+
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
